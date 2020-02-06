9184ce264eeb4d5e2ac2c4884b4b3e48e5eaf799
sammy njihia <sammy.njihia@jumo.world>
2020-02-06 13:50:27 +0300

    Created the Book JPA entity

---
+++ src/main/kotlin/com/shelf/book/model/Book.kt
@@ -0,0 +1,19 @@
+package com.shelf.book.model
+
+import org.springframework.data.annotation.CreatedDate
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Book (
+        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long,
+        val author: String,
+        val title: String,
+        val publishedAt: LocalDateTime,
+        val synopsis: String,
+        val createdDate: CreatedDate
+)
