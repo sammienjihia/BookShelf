16fa83aac5c3e9b5af4c414d957a4ae215abd3f3
sammy njihia <sammy.njihia@jumo.world>
2020-02-07 14:19:11 +0300

    Added a createBookService to handle the creation of book. NB Seperation of concerns between model and business logic

--- src/main/kotlin/com/shelf/book/application/createBook/CreateBookService.kt
+++ src/main/kotlin/com/shelf/book/application/createBook/CreateBookService.kt
@@ -1,2 +1,24 @@
 package com.shelf.book.application.createBook
 
+import com.shelf.book.infrastructure.factory.GenericDataMapper
+import com.shelf.book.model.BookRepository
+import com.shelf.book.model.domain.Book
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class CreateBookService {
+
+    @Autowired
+    private lateinit var bookRepository: BookRepository
+
+    val dataMapper = GenericDataMapper
+
+    fun createBook(book: Book): Book{
+
+        this.bookRepository.save(com.shelf.book.model.Book().apply {
+            author = book.author
+        })
+    }
+
+}
