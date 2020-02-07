7a82ba39d0335db7772b995f68c8ca505f5b611e
sammy njihia <sammy.njihia@jumo.world>
2020-02-07 15:58:12 +0300

    Added the CreateBookservice

--- src/main/kotlin/com/shelf/book/application/createBook/CreateBookService.kt
+++ src/main/kotlin/com/shelf/book/application/createBook/CreateBookService.kt
@@ -1,10 +1,11 @@
 package com.shelf.book.application.createBook
 
-import com.shelf.book.infrastructure.factory.GenericDataMapper
+import com.shelf.book.application.createBook.exceptions.CreateBookServiceException
 import com.shelf.book.model.BookRepository
 import com.shelf.book.model.domain.Book
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.lang.Exception
 
 @Service
 class CreateBookService {
@@ -12,13 +13,22 @@ class CreateBookService {
     @Autowired
     private lateinit var bookRepository: BookRepository
 
-    val dataMapper = GenericDataMapper
 
+    @Throws(CreateBookServiceException::class)
     fun createBook(book: Book): Book{
 
-        this.bookRepository.save(com.shelf.book.model.Book().apply {
-            author = book.author
-        })
+        try {
+            this.bookRepository.save(com.shelf.book.model.Book().apply {
+                author = book.author!!
+                publishedAt = book.publishedAt!!
+                synopsis = book.synopsis!!
+                title = book.title!!
+            })
+        }catch (ex: Exception){
+            throw CreateBookServiceException("An error ${ex.message} has occured", ex)
+        }
+
+        return book
     }
 
 }
