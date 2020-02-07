8152f750f12e6543756060a2fc21e21a13855d22
sammy njihia <sammy.njihia@jumo.world>
2020-02-07 15:57:39 +0300

    Created the CreateBookServiceException

--- src/main/kotlin/com/shelf/book/application/createBook/exceptions/CreateBookServiceException.kt
+++ src/main/kotlin/com/shelf/book/application/createBook/exceptions/CreateBookServiceException.kt
@@ -1,2 +1,5 @@
 package com.shelf.book.application.createBook.exceptions
 
+import java.lang.Exception
+
+class CreateBookServiceException (message: String, cause: Throwable?): Exception(message, cause)
